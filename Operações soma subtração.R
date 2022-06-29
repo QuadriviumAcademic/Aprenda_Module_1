227cc533fd2e8fc828b727677c41a7483cb1e1e5
Quadrivium Academic <quadriviumacademic@protonmail.com>
2022-06-29 15:37:49 -0300

    Add files via upload

---
+++ Operações soma subtração.R
@@ -0,0 +1,53 @@
+#Operações matemáticas com o R Studio
+
+#1) SOMA
+
+3 + 4 + 5
+2 + 3 + 123
+4 + 7 + 9 + 10 + 34
+
+#1.1) Armazenando resultados em um objeto
+
+soma1 = 3 + 4 + 5 # ou soma1 <- 3 + 4 + 5
+soma1
+
+
+soma2 = 2 + 3 + 123 # ou soma2 <- 2 + 3 + 123
+soma2
+
+soma3 = 4 + 7 + 9 + 10 + 34 # ou soma 3 <- 4 + 7 + 9 + 10 + 34
+soma3
+
+#1.2) Expressões algébricas
+
+algebra1 = (1 + 3) + 5
+algebra1
+
+algebra2 = (1 + 3) + (5 + 10)
+algebra2
+
+algebra3 = ((1 + 3) + (5 + 10)) + 1
+algebra3
+
+algebra4 = algebra2 + 1
+algebra4
+
+algebra5 = algebra1 + algebra2 + algebra3 + algebra4
+algebra5
+
+#2) Subtração
+
+subtracao1 = 100 - 70
+subtracao1
+
+subtracao2 = 1030 - 791
+subtracao2
+
+subtracao3 = 345.76 - 567.91
+subtracao3
+
+subtracao4 = subtracao1 + subtracao2 + subtracao3
+subtracao4
+
+subtracao5 = ((subtracao1 - subtracao2)-subtracao3) - subtracao4
+subtracao5
