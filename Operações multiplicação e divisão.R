9a6b5c0ba150b05f2ac74ff7d4bba96f80c88c2c
Quadrivium Academic <quadriviumacademic@protonmail.com>
2022-06-30 15:23:24 -0300

    Add files via upload

---
+++ Operações multiplicação e divisão.R
@@ -0,0 +1,22 @@
+#3) Multiplicação
+
+multi1 = 3 * 4
+multi1
+
+multi2 = ((multi1 * 12) + (34 - 12.79))
+multi2
+
+4# Divisão
+
+div1 = 100 / 2
+div1
+
+div2 = (multi2 / multi1)
+div2
+
+div3 = ((multi1 + multi2) * 3) + (multi2 / 4.88)
+div3
+
+
+
+
