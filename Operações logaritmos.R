785fb428b748378879eaaa2cb7322cd6cc095f0d
Quadrivium Academic <quadriviumacademic@protonmail.com>
2022-06-30 15:49:38 -0300

    Add files via upload

---
+++ Operações logaritmos.R
@@ -0,0 +1,53 @@
+#7) Logaritmos
+
+# Logaritmos são operadores de potência. 
+
+# Exemplo 1: log3 ->  10 ^ x = 3
+# Exemplo 2: log2 (3) -> 2 ^ x = 3
+
+
+#7.1) Logaritmos na base 2
+
+loga = log(3, 2)
+loga
+
+conferencia = 2 ^ 1.584963
+conferencia
+
+logvar1 = log2(3)
+logvar1
+
+logb = log(123.765, 2)
+logb
+
+logvar2 = log2(123.765)
+logvar2
+
+
+#7.2) Logaritmos na base 10
+
+logc = log(4.67, 10)
+logc
+
+logvar3 = log10(4.67)
+logvar3
+
+logd = log(7.89, 10) + log(5.67, 2)
+logd
+
+
+#7.3) Logaritmos naturais (base = e)
+
+#Definição: log (y) = e ^ x = y
+
+
+lognat1 = log(45)
+lognat1
+
+lognat2 = log(456.248)
+lognat2
+
+
+
+
+
