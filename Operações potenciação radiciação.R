e3789ede815259e5aed0e8277f39403da1004fdd
Quadrivium Academic <quadriviumacademic@protonmail.com>
2022-06-30 15:29:53 -0300

    Add files via upload

---
+++ Operações potenciação radiciação.R
@@ -0,0 +1,35 @@
+#5) Potenciação
+
+potencia1 = 3 ** 2 # ou: potencia1 = 3 ^ 2
+potencia1
+
+
+resultado = (((potencia1 * 123.456) + (12.79 ^ 3)) - ((45 * 9) / 4))
+resultado
+
+#6) Radiciação
+
+raiz1 = sqrt(100)
+raiz1
+
+raiz2 = sqrt(125.9876)
+raiz2
+
+resultadonovo = raiz1 ** raiz2
+resultadonovo
+
+#6.1) E se eu quiser uma raiz cúbica ou de ordem superior?
+
+# raiz quadrada de x = x ^ (1/2)
+# raiz cúbica de x = x ^ (1/3)
+# raiz quarta de x = x ^ (1/4)
+# raiz n de x = x ^ (1/n)
+
+raizcubica = 45 ^ (1/3)
+raizcubica
+
+raizquarta = 45 ^ (1/4)
+raizquarta
+
+raizdecima = 45 ^ (1/10)
+raizdecima
